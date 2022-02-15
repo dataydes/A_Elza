eef7c0c70bcc0d0ac3f69e2af37af06884cfc288
Daniel Ataydes <daniel_ataydes@msn.com>
2022-02-15 18:40:56 -0300

    Update Install_Elza.bat
    updated selenium

--- Install_Elza.bat
+++ Install_Elza.bat
@@ -1,6 +1,6 @@
 echo "Instalando pacotes necessários"
 pip3 install urllib3
-pip3 install selenium
+pip3 install selenium==3.141.0
 pip3 install html5lib
 pip3 install requests
 python -m pip install --upgrade pip
