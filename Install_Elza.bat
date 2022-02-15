c11f2bab5ccf911150e4a1146a74aeb81f550571
Daniel Ataydes <daniel_ataydes@msn.com>
2022-02-15 18:28:00 -0300

    Updated commands
    Updated Elza commands

--- Install_Elza.bat
+++ Install_Elza.bat
@@ -3,6 +3,6 @@ pip3 install urllib3
 pip3 install selenium
 pip3 install html5lib
 pip3 install requests
-python -m pip install upgrade pip
+python -m pip install --upgrade pip
 cd src
 start streamlink-2.2.0.exe
