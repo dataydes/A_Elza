56461235a68b1eea82b86d1b861d8118e97f4600
Daniel Ataydes <daniel_ataydes@msn.com>
2022-02-16 14:00:30 -0300

    Install update
    updeted install bat, add chorme version and automatic download Chrome Driver.

--- Install_Elza.bat
+++ Install_Elza.bat
@@ -1,8 +1,12 @@
 echo "Instalando pacotes necessários"
-pip3 install urllib3
-pip3 install selenium==3.141.0
-pip3 install html5lib
-pip3 install requests
+pip install urllib3
+pip install selenium==3.141.0
+pip install html5lib
+pip install requests
+pip install pywin32
 python -m pip install --upgrade pip
+python Update_ChormeDriver.py
 cd src
 start streamlink-2.2.0.exe
+cd ..
+del chromedriver.zip
---
+++ Update_ChormeDriver.py
@@ -0,0 +1,23 @@
+from win32com.client import Dispatch
+import subprocess 
+from zipfile import ZipFile
+import os
+
+def get_version_via_com(filename):  #Pega a versão do Chorme instalada
+    parser = Dispatch("Scripting.FileSystemObject")
+    try:
+        version = parser.GetFileVersion(filename)
+    except Exception:
+        return None
+    return version
+
+if __name__ == "__main__":
+    paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+             r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
+    version = list(filter(None, [get_version_via_com(p) for p in paths]))[0]
+    print(version)  #imprime a versão do Chrome instalada.
+    cmd = "Invoke-WebRequest https://chromedriver.storage.googleapis.com/"+ version +"/chromedriver_win32.zip -OutFile chromedriver.zip"
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    zip = ZipFile('chromedriver.zip')   #Extrai o pacote.
+    zip.extractall()
+    
